c1e02caf5cb2f58f9c550554a480b829fb3da843
JIN <63086170+kjh-icnl@users.noreply.github.com>
2024-08-02 03:33:18 +0900

    Fix typo

--- Multiprocessing_2/raw/utils.py
+++ Multiprocessing_2/raw/utils.py
@@ -6,16 +6,16 @@ def log_cpu_pid(p_sec, c_sec=None) -> None:
     # process section
     # core section
     
-    os = platform.system()
+    os_system = platform.system()
 
     # Windows
-    if os == 'Windows':
+    if os_system == 'Windows':
         print(f'{p_sec}: {os.getpid()}')
     # MacOS
-    elif os == 'Darwin':
+    elif os_system == 'Darwin':
         print(f'{p_sec}: {os.getpid()}')
     # Linux
-    elif os == 'Linux':
+    elif os_system == 'Linux':
         psp = psutil.Process()
         print(f'{p_sec}: {os.getpid()} / {c_sec}: {psp.cpu_num()}')
     # Others, unknown
